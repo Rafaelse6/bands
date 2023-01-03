97b209ff85da4c2e374a44c8ce8c19884f3efaf2
Rafael Santos <rafa.05125@gmail.com>
2023-01-03 19:27:01 -0300

    Project created

---
+++ bands/src/test/java/com/rafaelsantos/bands/BandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.bands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
